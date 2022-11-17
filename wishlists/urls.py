c99cdea0f75cba11e5a73002b68bded420b5fd40
Yebin Kim <rladpqls07@gmail.com>
2022-11-18 00:35:09 +0900

    feat: add PUT method for rooms in wishlist

--- wishlists/urls.py
+++ wishlists/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.Wishlists.as_view()),
     path("<int:pk>", views.WishlistDetail.as_view()),
+    path("<int:pk>/rooms/<int:room_pk>", views.WishlistRoomToggle.as_view()),
 ]
 
--- wishlists/views.py
+++ wishlists/views.py
@@ -4,6 +4,7 @@ from rest_framework.status import HTTP_200_OK
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.exceptions import NotFound
 from .models import Wishlist
+from rooms.models import Room
 from .serializers import WishlistSerializer
 
 
@@ -53,4 +54,37 @@ class WishlistDetail(APIView):
         wishlist = self.get_object(pk, request.user)
         wishlist.delete()
         return Response(status=HTTP_200_OK)
+  
+
+class WishlistRoomToggle(APIView):
     
+    """ wishlist의 rooms 리스트에 room을 추가하고, 이미 해당 room이 존재하면 삭제하는 view """
+    
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    
+    def get_list(self, pk, user):
+        
+        """  room을 추가/삭제할 wishlist를 찾는 메소드 """
+        
+        try:
+            return Wishlist.objects.get(pk=pk, user=user)
+        except Wishlist.DoesNotExist:
+            raise NotFound
+    
+    def get_room(self, pk):
+        
+        """ wishlist에 추가/삭제할 room을 찾는 메소드 """
+        
+        try:
+            return Room.objects.get(pk=pk)
+        except Room.DoesNotExist:
+            raise NotFound
+    
+    def put(self, request, pk, room_pk):
+        wishlist = self.get_list(pk, request.user)
+        room = self.get_room(room_pk)
+        if wishlist.rooms.filter(pk=room.pk).exists():
+            wishlist.rooms.remove(room)
+        else:
+            wishlist.rooms.add(room)
+        return Response(status=HTTP_200_OK)
