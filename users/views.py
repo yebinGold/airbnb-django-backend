d45cc9369f47dfcc77e72e661e4d27e62468ffe0
Yebin Kim <rladpqls07@gmail.com>
2022-11-18 15:57:21 +0900

    feat: add GET, PUT  methods for user profile

--- users/views.py
+++ users/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .serializers import PrivateUserSerializer
 
-# Create your views here.
+class Me(APIView):
+    
+    """ My Profile """
+    
+    permission_classes = [IsAuthenticated] # 내 프로필 정보를 보고 수정 -> 나만 가능(private url)
+    
+    def get(self, request):
+        user = request.user
+        return Response(PrivateUserSerializer(user).data)
+    
+    def put(self, request):
+        user = request.user
+        serializer = PrivateUserSerializer(user, data=request.data, partial=True)
+        if serializer.is_valid():
+            updated_user = serializer.save()
+            return Response(PrivateUserSerializer(updated_user).data)
+        else:
+            return Response(serializer.errors)
