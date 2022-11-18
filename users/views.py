13863a2576052630ae438029577b5ac754dc9d0f
Yebin Kim <rladpqls07@gmail.com>
2022-11-18 16:20:28 +0900

    feat: add POST method for user account

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import ParseError
 from .serializers import PrivateUserSerializer
 
 class Me(APIView):
@@ -19,5 +20,23 @@ class Me(APIView):
         if serializer.is_valid():
             updated_user = serializer.save()
             return Response(PrivateUserSerializer(updated_user).data)
+        else:
+            return Response(serializer.errors)
+
+
+class Users(APIView):
+    
+    """ Create User Account """
+
+    def post(self, request):
+        password = request.data.get("password")
+        if not password:
+            raise ParseError("Check your password.")
+        serializer = PrivateUserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            user.set_password(password)
+            user.save()
+            return Response(PrivateUserSerializer(user).data)
         else:
             return Response(serializer.errors)
