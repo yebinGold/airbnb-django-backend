653339aec54bcf50ed1b6a5d66ec0c7cbf6c77f6
Yebin Kim <rladpqls07@gmail.com>
2022-11-18 16:51:17 +0900

    feat: add PUT method for changing password

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", views.Users.as_view()),
     path("me", views.Me.as_view()),
     path("@<str:username>", views.PublicUser.as_view()), # /me url path와 user 'me'를 구분하기 위한 @
+    path("change-password", views.ChangePassword.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import ParseError, NotFound
+from rest_framework import status
 from .serializers import PrivateUserSerializer, PublicUserSerializer
 from .models import User
 
@@ -54,4 +55,23 @@ class PublicUser(APIView):
             raise NotFound    
         serializer = PublicUserSerializer(user)
         return Response(serializer.data)
+
+
+class ChangePassword(APIView):
+    
+    permission_classes = [IsAuthenticated]
+    
+    def put(self, request):
+        user = request.user
+        old_password = request.data.get('old-password')
+        new_password = request.data.get('new-password')
+        if not old_password or not new_password: # 데이터 없으면
+            raise ParseError
+        if user.check_password(old_password):
+            user.set_password(new_password)
+            user.save()
+            return Response(status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
         
+
