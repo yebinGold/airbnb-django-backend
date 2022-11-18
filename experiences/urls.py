7b28ba20780e907ea862bd34b557f70755ff6d2a
Yebin Kim <rladpqls07@gmail.com>
2022-11-19 01:11:48 +0900

    feat: build experience API url

--- experiences/urls.py
+++ experiences/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.Experiences.as_view()),
+    path("<int:pk>", views.ExperienceDetail.as_view()),
     path("perks/", views.Perks.as_view()),
     path("perks/<int:pk>", views.PerkDetail.as_view())
 ]
--- experiences/views.py
+++ experiences/views.py
@@ -1,23 +1,24 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.exceptions import NotFound
+from rest_framework.exceptions import NotFound, ParseError
 from rest_framework.status import HTTP_204_NO_CONTENT
-from .serializers import PerkSerializer
-from .models import Perk
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from . import serializers
+from .models import Perk, Experience
 
 
 class Perks(APIView):
     
     def get(self, request):
         all_perks = Perk.objects.all()
-        serlalizer = PerkSerializer(all_perks, many=True)
+        serlalizer = serializers.PerkSerializer(all_perks, many=True)
         return Response(serlalizer.data)
         
     def post(self, request):
-        serializer = PerkSerializer(data=request.data)
+        serializer = serializers.PerkSerializer(data=request.data)
         if serializer.is_valid():
             new_perk = serializer.save()
-            return Response(PerkSerializer(new_perk).data)
+            return Response(serializers.PerkSerializer(new_perk).data)
         else:
             return Response(serializer.errors)
 
@@ -33,15 +34,15 @@ class PerkDetail(APIView):
     
     def get(self, request, pk):
         perk = self.get_object(pk)
-        serializer = PerkSerializer(perk)
+        serializer = serializers.PerkSerializer(perk)
         return Response(serializer.data)
         
     def put(self, request, pk):
         perk = self.get_object(pk)
-        serializer = PerkSerializer(perk, data=request.data, partial=True)
+        serializer = serializers.PerkSerializer(perk, data=request.data, partial=True)
         if serializer.is_valid():
             updated_perk = serializer.save()
-            return Response(PerkSerializer(updated_perk).data)
+            return Response(serializers.PerkSerializer(updated_perk).data)
         else:
             return Response(serializer.errors)
         
@@ -49,3 +50,44 @@ class PerkDetail(APIView):
         perk = self.get_object(pk)
         perk.delete()
         return Response(status=HTTP_204_NO_CONTENT)
+    
+    
+class Experiences(APIView):
+    
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    
+    def get(self, request):
+        all_experiences = Experience.objects.all()
+        serializer = serializers.ExperienceListSerializer(all_experiences, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+ 
+        serializer = serializers.ExperienceDetailSerializer(data=request.data)
+        if serializer.is_valid():
+            new_experience = serializer.save(host=request.user)
+            return Response(serializers.ExperienceDetailSerializer(new_experience).data)
+        else:
+            return Response(serializer.errors)
+        
+        
+class ExperienceDetail(APIView):
+    
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    
+    def get_object(self, pk):
+        try:
+            return Experience.objects.get(pk=pk)
+        except Experience.DoesNotExist:
+            raise NotFound
+    
+    def get(self, request, pk):
+        experience = self.get_object(pk)
+        serializer = serializers.ExperienceDetailSerializer(experience)
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        pass
+    
+    def delete(self, request, pk):
+        pass
