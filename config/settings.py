8f1916959161e456cb5db6f9749be31dd8684e97
Yebin Kim <rladpqls07@gmail.com>
2022-11-21 02:02:42 +0900

    refactor: hide secret key

--- config/settings.py
+++ config/settings.py
@@ -11,16 +11,21 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
+
+env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+environ.Env.read_env(os.path.join(BASE_DIR, ".env")) # env 파일 경로 적어주기
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yj81lu!atyrb*k7##yf)3o)@l08g5p0elxz=w@q43*b5izvk^u'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
